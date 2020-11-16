5aa7250c872f820a252b3649d8869aba9cc7fc81
Anonymous guy <anonguy912@gmail.com>
2020-11-16 20:22:26 +0530

    fixed register/login creds

--- auth/db-connect.php
+++ auth/db-connect.php
@@ -1,5 +1,11 @@
 <?php
-    @ $mysqli = new mysqli("address", "username", "password", "db");
+    //database credencials
+    $db_server_name="localhost";
+    $db_username=getenv('DB_USER');
+    $db_password=getenv('DB_PASSWORD');
+    $db_name="webdvxms_WebDevChat";
+
+    $mysqli = new mysqli($db_server_name, $db_username, $db_password, $db_name);
 
     if($mysqli -> connect_errno) {
         $response['db_err'] = "Could not connect to database at the moment.<br>Please try again later.";
